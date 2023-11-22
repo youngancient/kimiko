0ba7abbb46e3f7bacd78618e2b8d0439a98d2807
Jude Tochy <tochyokoye@gmail.com>
2023-11-22 21:39:29 +0100

    project scaffold

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import Layout from '@/components/Layout/Layout'
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Layout>
+      <>Header</>
+      <Component {...pageProps} />
+      <>Footer</>
+    </Layout>
+  )
+}
---
+++ src/styles/layoutStyles.tsx
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const LayoutStyles = styled.div`
+  border: 2px solid #000;
+`;
